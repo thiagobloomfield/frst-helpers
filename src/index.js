fd74d20289d7272dff9e3569eb6ea753792fbb14
Thiago Bloomfield <thiagobloomf@gmail.com>
2021-05-21 10:57:54 -0300

    Verify JWT

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const { VerifyJWT } = require('./middlewares/verify-jwt.middleware');
 const { ModelUtils } = require('./model-utils');
 const { MailService } = require('./services/mail.service');
 
 module.exports = {
   MailService: MailService,
-  ModelUtils: ModelUtils
+  ModelUtils: ModelUtils,
+  VerifyJWT: VerifyJWT
 }
